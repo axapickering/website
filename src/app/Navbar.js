5f800c4d04009a4f9916310016b39a2ee241ef1a
Axa Pickering <axapickering@gmail.com>
2024-05-09 15:29:40 -0700

    added bootstrap and navbar

---
+++ src/app/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return <nav class="nav flex-column">
+  <a class="nav-link active fs-1" aria-current="page" href="/">Axa Pickering</a>
+  <a class="nav-link" href="/projects">Projects</a>
+  <a class="nav-link" href="/about-me">About me</a>
+  <a class="nav-link" href="/experience">Experience</a>
+</nav>
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import { Inter } from "next/font/google";
 import "./globals.css";
+import BootstrapClient from './BootstrapClient';
+import Navbar from './Navbar';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar/>
+        {children}
+        <BootstrapClient/>
+        </body>
     </html>
   );
 }
